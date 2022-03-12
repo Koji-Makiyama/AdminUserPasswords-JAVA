2bfacda621812a1862fa69f6ff8baff6c393ddad
Alexander Koji Makiyama <98917941+Koji-Makiyama@users.noreply.github.com>
2022-03-12 14:46:47 -0700

    Update User.java

--- User.java
+++ User.java
@@ -2,12 +2,18 @@ package com.AdminProject.main;
 import java.util.Scanner;
 
 class User {
+    public static String username;
     public String userpw;
+
     public static void setUserPw(Scanner o) {
-        User myUser = new User();
-        System.out.println("User class reached. Enter your desired User password.");
-        String userScan = o.next();
-        myUser.userpw = userScan;
-        System.out.println("User password set to " + myUser.userpw + ".");
+        User u01 = new User();
+        System.out.println("Enter your desired User name.");
+        String userScan1 = o.next();
+        u01.username = userScan1;
+        System.out.println("Admin class created. Enter your desired Admin password for " + userScan1 + ".");
+        String adminScan2 = o.next();
+        u01.userpw = adminScan2;
+        System.out.println("Admin password set to " + adminScan2 + " for " + userScan1 + ".");
+        Main.userlist.add(u01);
     }
 }
