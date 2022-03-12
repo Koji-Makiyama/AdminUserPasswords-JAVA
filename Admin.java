82461b14e788f93c269895a98e4d02f643190fee
Alexander Koji Makiyama <98917941+Koji-Makiyama@users.noreply.github.com>
2022-03-12 13:58:08 -0700

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,14 +1,13 @@
+package com.AdminProject.main;
 import java.util.Scanner;
 
-class Admin {
-    public static String adminname = "Administrator";
-    public String adminpw;
-    public static void setAdminPw() {
-        Admin myAdmin = new Admin();
-        System.out.println("Admin class reached. Enter your desired Admin password.");
-        Scanner scanner = new Scanner(System.in);
-        String adminScan = scanner.next();
-        myAdmin.adminpw = adminScan;
-        System.out.println("Admin password set to " + myAdmin.adminpw + ".");
+class User {
+    public String userpw;
+    public static void setUserPw(Scanner o) {
+        User myUser = new User();
+        System.out.println("User class reached. Enter your desired User password.");
+        String userScan = o.next();
+        myUser.userpw = userScan;
+        System.out.println("User password set to " + myUser.userpw + ".");
     }
 }
