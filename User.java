435b4ad3256fd0f4895ed51d76d886388b08ebe7
Alexander Koji Makiyama <98917941+Koji-Makiyama@users.noreply.github.com>
2022-03-12 13:59:12 -0700

    Update User.java

--- User.java
+++ User.java
@@ -1,12 +1,12 @@
+package com.AdminProject.main;
 import java.util.Scanner;
 
 class User {
-    public String userpw; 
-    public static void setUserPw() {
+    public String userpw;
+    public static void setUserPw(Scanner o) {
         User myUser = new User();
         System.out.println("User class reached. Enter your desired User password.");
-        Scanner scanner = new Scanner(System.in);
-        String userScan = scanner.next();
+        String userScan = o.next();
         myUser.userpw = userScan;
         System.out.println("User password set to " + myUser.userpw + ".");
     }
